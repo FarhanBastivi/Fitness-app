0c3995afea09704fb50a1031ca1072081bee97e6
Mohammed Farhan Bastivi <149455322+FarhanBastivi@users.noreply.github.com>
2025-04-08 22:37:13 +0530

    SearchExercises.js

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -9,19 +9,19 @@ const SearchExercises = ({setExercises,bodyPart,setBodyPart}) => {
   const [search,setSearch] = useState('');
   const [bodyParts,setBodyParts] = useState([])
 
-  // useEffect(()=>{
-  //   const fetchExercisesData = async () => {
-  //     try {
-  //       const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
-  //       if (bodyPartsData) {
-  //         setBodyParts(['all',...bodyPartsData]);  
-  //       }
-  //     } catch (error) {
-  //       console.error("Error fetching body parts:", error);
-  //     }
-  //   };
-  //   fetchExercisesData();
-  // }, []);
+  useEffect(()=>{
+    const fetchExercisesData = async () => {
+      try {
+        const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+        if (bodyPartsData) {
+          setBodyParts(['all',...bodyPartsData]);  
+        }
+      } catch (error) {
+        console.error("Error fetching body parts:", error);
+      }
+    };
+    fetchExercisesData();
+  }, []);
 
   const handleSearch = async() =>{
     if (search){
